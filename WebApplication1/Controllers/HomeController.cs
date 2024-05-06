9191aa3b9eef0df76bf636cff075520ee0f8befd
FeridBuludlu <buludluferid@gmail.com>
2024-05-06 13:18:16 +0400

    task added

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+        public   IActionResult  Index()
+        {
+            return View();
+
+
+
+        }
+
+
+        public IActionResult Index1()
+        {
+            Student student1 = new Student()
+            {
+                Id = 1,
+                Name = "Ferid"
+            };
+            Student studen2 = new Student()
+            {
+                Id = 2,
+                Name = "Namiq"
+
+            };
+            Student student3 = new Student()
+            {
+                Id = 3,
+                Name = "Natiq"
+            };
+
+
+            List<Student> students = new List<Student>();
+            students.Add(student1);
+            students.Add(studen2);
+            students.Add(student3);
+
+            return View(students);
+
+        }
+     
+
+
+
+
+
+
+    }
+}
